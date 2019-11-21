51ffb086af865e1cb093ed43cdf5bc0b7112e444
Anna Wijetunga <annawijetunga@gmail.com>
2019-11-21 21:24:46 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,6 @@ class Triangle
   end 
 
   def kind
-    
     if (s1 * s2 * s3) == 0 || (s1 + s2) <= s3 || (s2 + s3) <= s1 || (s3 + s1) <= s2
       begin
         raise TriangleError
@@ -26,7 +25,6 @@ class Triangle
       self.kind = :scalene
     end
   end 
-
 end
 
 class TriangleError < StandardError
