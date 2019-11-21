9c0d607b3d3ff7c6c7dd4d3daa8b2c4acf94b2f9
Anna Wijetunga <annawijetunga@gmail.com>
2019-11-21 20:54:21 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -4,9 +4,9 @@ class Triangle
   attr_reader :s1, :s2, :s3 
 
   def initialize(s1, s2, s3)
-    @s_1 = s_1
-    @s_2 = s_2
-    @s_3 = s_3
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3
   end 
 
   def kind
